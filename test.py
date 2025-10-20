272de4fd42a60169b6b3a2132df16e2153a9c280
Evamatrix <102815074+Evamatrix@users.noreply.github.com>
2025-10-20 12:37:24 -0500

    Created Landing Page

---
+++ test.py
@@ -0,0 +1,10 @@
+import streamlit as st
+ 
+st.title('GDPredict') # Project Title
+
+st.sidebar.title('Navigation')
+st.sidebar.markdown("""
+- **Home**: Overview of the app
+- **Predict GDP**: Input economic indicators to predict GDP
+- **About**: Information about the app and its creators
+""")
