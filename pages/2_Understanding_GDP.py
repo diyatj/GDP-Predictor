88ec5b124fd11e598a13a57b653b3180a4325534
Evamatrix <102815074+Evamatrix@users.noreply.github.com>
2025-10-29 16:42:26 -0500

    Added info
    Added detailed explanations for each component of the GDP equation, including Consumption, Investment, Government spending, and Exports-Imports.

--- pages/2_Understanding_GDP.py
+++ pages/2_Understanding_GDP.py
@@ -1,7 +1,34 @@
 import streamlit as st
 
 st.markdown("# Understanding GDP")
+# Write GDP Equation
+st.markdown("## GDP Equation: GDP = C + I + G + (X - M)")
+
+# Explain each component of the GDP equation
 st.write(
-    """We like money!"""
+    """
+1. Consumption (C)
+Consumption represents the sum of goods and services purchased by citizens—such as retail items or rent—and it grows as more is consumed. It’s the largest component of GDP. Typically, professionals view a steadily increasing consumption as a sign of a healthy economy because it signifies consumer confidence in spending versus uncertainty in the future and lack of spending.
+
+2. Investment (I)
+Investment refers to any domestic investment, or capital expenditures, in new assets that will provide future benefits. To invest in business activity, companies spend money on purchasing equipment, inventory, and building new establishments. The difference between consumption and investment is the period over which the purchased good or service provides benefits to its purchaser.
+
+Investment is important because higher levels of it increase productive capacity and boost employment rates.
+
+3. Government (G)
+Government represents the money (consumption expenditure and gross investment) spent by the government on goods and services, such as education, transportation, military, or infrastructure. This spending is funded by taxes and companies or borrowed. To run at a surplus instead of a deficit, the government needs to collect more money than it spends.
+
+Government spending becomes even more important to consider in the wake of a recession when consumer spending and business investment dramatically decline.
+
+4. Exports - Imports (X-M)
+The exports – imports piece of the equation refers to the exports of goods and services produced within the domestic economy and sold abroad, minus the imports purchased by domestic consumers. This includes all expenditures by companies geographically located within the country.
+
+If the country’s export (X) is greater than the value of its imports (M), the net value is positive, and the country has a trade surplus. Likewise, if M is greater than X, the country is running a trade deficit.
+
+Keep in mind that GDP ratios aren’t directly comparable between the United States and other countries, but the equation, metrics, and information used to calculate GDP are similar across the globe.
+
+Information from https://online.hbs.edu/blog/post/why-is-gdp-important
+
+"""
 )
 
