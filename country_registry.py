0a9ad12e97d6cc77d875d849010dfef1e646a63d
Diya Jibu <63443155+diyatj@users.noreply.github.com>
2025-11-14 14:57:07 -0600

    Added Japan in country_registry.py

--- country_registry.py
+++ country_registry.py
@@ -4,6 +4,7 @@ from typing import List, Optional
 # Simple registry mapping country names to CSV filenames.
 COUNTRY_CSV_MAP = {
     "United States": "datasets/US_GDP.csv",
+    "Japan" : "datasets/Japan_GDP.csv",
 }
 
 
