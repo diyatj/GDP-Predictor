93501e56a791921a41daaf81789813b90d147112
Evelyn Tran <evelyntran224@gmail.com>
2025-11-14 14:09:33 -0600

    Changed app logo

--- Home.py
+++ Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="GDPredict", # Web app title
-    page_icon="👋",
+    page_icon="assets/GDPredict Logo.svg",    # Web app icon
 )
 
 st.write("# GDPredict")
